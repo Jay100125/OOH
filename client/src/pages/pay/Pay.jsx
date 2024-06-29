5daf33362676bf87679cdc64350350685bb23d77
Jay100125 <jaypatel100125@gmail.com>
2024-06-29 13:59:36 +0530

    Payment Gatway

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -6,44 +6,48 @@ import newRequest from "../../utils/newRequest";
 import { useParams } from "react-router-dom";
 import CheckoutForm from "../../components/checkOutForm/CheckOutForm";
 
-import "./Pay.scss"
+import "./Pay.scss";
 
-
-const stripePromise = loadStripe("pk_test_51OckA4SHsFY4PlwpgY9kPflJRDojmLvtlincLAEHqgjIwXzy1P6eiSlGsLwRbUpc72LutP3izWuEjnp5EJa2uZKe00u81CCwOu");
+const stripePromise = loadStripe(
+  "pk_test_51OckA4SHsFY4PlwpgY9kPflJRDojmLvtlincLAEHqgjIwXzy1P6eiSlGsLwRbUpc72LutP3izWuEjnp5EJa2uZKe00u81CCwOu"
+);
 
 const Pay = () => {
-    const [clientSecret, setClientSecret] = useState("");
-    const { id } = useParams();
+  const [clientSecret, setClientSecret] = useState("");
+  const { id } = useParams();
 
-    useEffect(() => {
-        const makeRequest = async () => {
-            try {
-                const res = await newRequest.post(
-                    `/orders/create-payment-intent/${id}`
-                );
-                setClientSecret(res.data.clientSecret);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        makeRequest();
-    }, []);
-
-    const appearance = {
-        theme: 'stripe',
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await newRequest.post(
+          `/orders/create-payment-intent/${id}`
+        );
+        console.log("API response:", res);
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.error("API request error:", err);
+      }
     };
-    const options = {
-        clientSecret,
-        appearance,
-    };
+    makeRequest();
+  }, []);
 
-    return <div className="pay">
-        {clientSecret && (
-            <Elements options={options} stripe={stripePromise}>
-                <CheckoutForm />
-            </Elements>
-        )}
-    </div>;
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="pay">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
 };
 
-export default Pay
+export default Pay;
