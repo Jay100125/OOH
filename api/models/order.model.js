71f220355cb8624b616f5a8fca5b3d93ee494238
Jay100125 <jaypatel100125@gmail.com>
2024-06-29 19:33:10 +0530

    oreder-model

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -18,14 +18,14 @@ const orderSchema = new Schema({
         type: Number,
         required: true,
     },
-    sellerId: {
-        type: String,
-        required: true,
-    },
-    buyerId: {
-        type: String,
-        required: true,
-    },
+    // sellerId: {
+    //     type: String,
+    //     required: true,
+    // },
+    // buyerId: {
+    //     type: String,
+    //     required: true,
+    // },
     isCompleted: {
         type: Boolean,
         default: false,
